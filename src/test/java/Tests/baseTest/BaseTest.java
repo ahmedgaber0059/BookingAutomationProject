ff561409f2a4a947bcbf245cab668d2464b114dc
ahmedgaber0059 <ahmedgaber0059@gmail.com>
2024-06-21 05:01:36 +0300

    run with chrome session

--- src/test/java/Tests/baseTest/BaseTest.java
+++ src/test/java/Tests/baseTest/BaseTest.java
@@ -14,49 +14,49 @@ import java.lang.reflect.Method;
 
 public class BaseTest {
     protected WebDriver driver;
-//
-//    @BeforeClass
-//    public void startDriver() {
-//        setBrowser("chrome");
-//        navigateTo();
-//        ExtentReport.startReporting();
-//    }
-//
-//    @BeforeMethod
-//    public void create (Method method){
-//        ExtentReport.createTest(method);
-//    }
-//
-//    @AfterMethod
-//    public void aftermethodT(ITestResult result) throws IOException {
-//        ScreenShot.takeScreenShot(driver);
-//        ExtentReport.logStatus(result, driver);
-//    }
-//
-//    private void navigateTo(){
-//        driver.get("https://www.booking.com/");
-//        driver.manage().window().maximize();
-//    }
-//
-//    private void setBrowser(String browser){
-//        switch (browser.toLowerCase()){
-//            case "chrome":
-//                WebDriverManager.chromedriver().setup();
-//
-//                driver = new ChromeDriver();
-//                break;
-//            case"edge":
-//                WebDriverManager.edgedriver().setup();
-//
-//                driver = new EdgeDriver();
-//                break;
-//        }
-//    }
-//
-//    @AfterClass
-//    public void teardown(){
-//        driver.quit();
-//        ExtentReport.flushReport();
-//    }
+
+    @BeforeClass
+    public void startDriver() {
+        setBrowser("chrome");
+        navigateTo();
+        ExtentReport.startReporting();
+    }
+
+    @BeforeMethod
+    public void create (Method method){
+        ExtentReport.createTest(method);
+    }
+
+    @AfterMethod
+    public void aftermethodT(ITestResult result) throws IOException {
+        ScreenShot.takeScreenShot(driver);
+        ExtentReport.logStatus(result, driver);
+    }
+
+    private void navigateTo(){
+        driver.get("https://www.booking.com/");
+        driver.manage().window().maximize();
+    }
+
+    private void setBrowser(String browser){
+        switch (browser.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+
+                driver = new ChromeDriver();
+                break;
+            case"edge":
+                WebDriverManager.edgedriver().setup();
+
+                driver = new EdgeDriver();
+                break;
+        }
+    }
+
+    @AfterClass
+    public void teardown(){
+        driver.quit();
+        ExtentReport.flushReport();
+    }
 
 }
--- src/test/java/Tests/regressionTest/RegressionTest.java
+++ src/test/java/Tests/regressionTest/RegressionTest.java
@@ -9,22 +9,17 @@ import org.testng.annotations.Test;
 public class RegressionTest extends BaseTest {
     HomePage homePage;
 
-//    @BeforeClass
-//    public void initializeObjects(){
-//         homePage = new HomePage(driver);
-//    }
-//
-//    @Test
-//    public void
-//    validateSearchFunctionality(){
-//        homePage.validateSearchElements();
-//        homePage.sendInputsToSearchBar("Cairo" , "Cairo Governorate, Egypt");
-//        homePage.selectCheckInAndCheckOutDate("2024-06-22", "2024-08-13");
-//        homePage.clickOnSearchButton();
-//    }
+    @BeforeClass
+    public void initializeObjects(){
+         homePage = new HomePage(driver);
+    }
 
     @Test
-    public void test(){
-        System.out.println("hello github action job");
+    public void
+    validateSearchFunctionality(){
+        homePage.validateSearchElements();
+        homePage.sendInputsToSearchBar("Cairo" , "Cairo Governorate, Egypt");
+        homePage.selectCheckInAndCheckOutDate("2024-06-22", "2024-08-13");
+        homePage.clickOnSearchButton();
     }
 }
